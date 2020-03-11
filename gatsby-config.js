2c950e68b2c1e2ee2712d1a51e9057ebbfb85af9
Marcello Paiva <marcello.paiva@gmail.com>
2020-03-11 11:08:56 -0400

    installed gatsby-plugin-react-axe

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,22 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-react-axe',
+      options: {
+        // Integrate react-axe in production. This defaults to false.
+        showInProduction: false,
+
+        // Options to pass to axe-core.
+        // See: https://github.com/dequelabs/axe-core/blob/master/doc/API.md#api-name-axeconfigure
+        axeOptions: {
+          // Your axe-core options.
+        },
+        // Context to pass to axe-core.
+        // See: https://github.com/dequelabs/axe-core/blob/master/doc/API.md#context-parameter
+        axeContext: {
+          // Your axe-core context.
+        }
+      },
+    },
+  ],
 }
