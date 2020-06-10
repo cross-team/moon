b64efc9747d949c6267217e47b59da3d89671b0c
Marcello Paiva <marcello.paiva@gmail.com>
2020-06-10 11:32:31 -0400

    fixed headings and refactored services section

--- src/pages/index.js
+++ src/pages/index.js
@@ -89,85 +89,76 @@ function Index({ data }) {
   //   return { ...imageSet, [key]: result }
   // }, {})
 
+  var servicesData = [
+    {
+      title: 'Accessibility',
+      content: `As the world changes to adjust to COVID-19, more than ever before,
+      businesses need clear Accessibility and Procurement policies in place.
+      Our team of Accessibility experts can get your organization started
+      with a proper and cost-effective Accessibility strategy.`,
+    },
+    {
+      title: 'Workshops',
+      content: `Enterprise training approach combines in-person instruction and online
+      learning with relevant content and an adaptive curriculum for User
+      Experience Design practices and Digital Accessibility.`,
+    },
+    {
+      title: 'User Research',
+      content: `We partner with a network of usability pool of participants covering
+      any demographic and disability needs.`,
+    },
+    {
+      title: 'Discovery',
+      content: `Dive into the idea, explore opportunities and assess the competitors
+      through Design Thinking methodologies.`,
+    },
+    {
+      title: 'Design',
+      content: `Experts in building a product visual language, design token
+      architecture and complete Design Systems. Expertly crafted bespoke
+      platforms built with Accessibility in mind from day-1 to scale and
+      succeed.`,
+    },
+    {
+      title: 'Development',
+      content: `Accessible front-end development ensures people with different
+      abilities can access, understand, and navigate web content, regardless
+      of how they’re accessing it. Our front-end developers collaborate with
+      other members of a cross-functional team to implement a robust user
+      experience.`,
+    },
+    {
+      title: 'Social Media',
+      content: `Digital Marketing is also maturing to become responsibly inclusive.
+      Our Social Media strategy is based on best practices to ensure that
+      your online content is usable and accessible to all citizens,
+      including those with disabilities.`,
+    },
+    {
+      title: 'Online Reputation',
+      content: `The recent wave of web accessibility lawsuits and scandals have put
+      organizations in reputation management crises and hefty legal
+      expenses. We have the process in place to help protecting your
+      branding and recovering from any negative social wave.`,
+    },
+    {
+      title: 'Influencer Marketing',
+      content: `Connecting your business with targeted audiences can be a waste of
+      investment. We can connect you with the right people that influence
+      your audience on a daily-basis, effectively conveying the values of
+      your brand.`,
+    },
+  ]
+
   var services = (
     <Grid container spacing={4} className={classes.services}>
-      <Grid item xs={12} md={4}>
-        <Typography variant="h3">Accessibility</Typography>
-        <Typography>
-          As the world changes to adjust to COVID-19, more than ever before,
-          businesses need clear Accessibility and Procurement policies in place.
-          Our team of Accessibility experts can get your organization started
-          with a proper and cost-effective Accessibility strategy.
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Typography variant="h3">Workshops</Typography>
-        <Typography>
-          Enterprise training approach combines in-person instruction and online
-          learning with relevant content and an adaptive curriculum for User
-          Experience Design practices and Digital Accessibility.
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Typography variant="h3">User Research</Typography>
-        <Typography>
-          We partner with a network of usability pool of participants covering
-          any demographic and disability needs.
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Typography variant="h3">Discovery</Typography>
-        <Typography>
-          Dive into the idea, explore opportunities and assess the competitors
-          through Design Thinking methodologies.
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Typography variant="h3">Design</Typography>
-        <Typography>
-          Experts in building a product visual language, design token
-          architecture and complete Design Systems. Expertly crafted bespoke
-          platforms built with Accessibility in mind from day-1 to scale and
-          succeed.
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Typography variant="h3">Development</Typography>
-        <Typography>
-          Accessible front-end development ensures people with different
-          abilities can access, understand, and navigate web content, regardless
-          of how they’re accessing it. Our front-end developers collaborate with
-          other members of a cross-functional team to implement a robust user
-          experience.
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Typography variant="h3">Social Media</Typography>
-        <Typography>
-          Digital Marketing is also maturing to become responsibly inclusive.
-          Our Social Media strategy is based on best practices to ensure that
-          your online content is usable and accessible to all citizens,
-          including those with disabilities.
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Typography variant="h3">Online Reputation</Typography>
-        <Typography>
-          The recent wave of web accessibility lawsuits and scandals have put
-          organizations in reputation management crises and hefty legal
-          expenses. We have the process in place to help protecting your
-          branding and recovering from any negative social wave.
-        </Typography>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Typography variant="h3">Influencer Marketing</Typography>
-        <Typography>
-          Connecting your business with targeted audiences can be a waste of
-          investment. We can connect you with the right people that influence
-          your audience on a daily-basis, effectively conveying the values of
-          your brand.
-        </Typography>
-      </Grid>
+      {servicesData.map(service => (
+        <Grid item xs={12} md={4}>
+          <h3 ariaLevel="2">{service.title}</h3>
+          <Typography>{service.content}</Typography>
+        </Grid>
+      ))}
     </Grid>
   )
 
