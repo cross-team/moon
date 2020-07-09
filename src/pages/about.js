ad7ae2d97df1d8f6aa97289bca81ff9d52d6fc98
Marcello Paiva <marcello.paiva@gmail.com>
2020-07-09 11:44:02 -0400

    added fix for disappearing content

--- src/pages/about.js
+++ src/pages/about.js
@@ -39,8 +39,6 @@ export var POSTS_QUERY = graphql`
 
 function About({ data }) {
   var classes = useStyles()
-  console.log(data)
-  console.log(data.github.repository.issues.nodes[0].bodyHTML)
   return (
     <>
       <Layout title="About">
@@ -53,11 +51,13 @@ function About({ data }) {
             direction="column"
             className={classes.contentContainer}
           >
-            <Typography
-              dangerouslySetInnerHTML={{
-                __html: data.github.repository.issues.nodes[0].bodyHTML,
-              }}
-            />
+            <Typography>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: data.github.repository.issues.nodes[0].bodyHTML,
+                }}
+              />
+            </Typography>
           </Grid>
         </Grid>
       </Layout>
